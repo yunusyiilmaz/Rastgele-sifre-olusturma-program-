323ca80851e2872689e1d7fa5fc371fcc40eebf3
yunusyiilmaz <yns.ylmz1381@gmail.com>
2022-08-12 18:30:22 +0300

    Rastgele şifre oluşturma programı
    Rastgele şifre oluşturma programı

---
+++ RASTGELE ŞİFRE OLUŞTURAN PROGRAM/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rastgele = new Random();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 8; i++)
+            {
+                int ascii = rastgele.Next(65, 127);
+                char karakter = Convert.ToChar(ascii);
+                sb.Append(karakter);
+
+            }
+            Console.WriteLine(sb.ToString());
+            Console.Read();
+        }
+        
+    }
+}
